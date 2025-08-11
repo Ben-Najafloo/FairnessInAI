c89ca8f1eefd7f9c7f469bbfb04b76ceb8673cb5
Ben Najafloo <ben@zerolatency.tv>
2025-08-11 18:19:03 +0200

    test typeScript

--- frontend/src/components/Test.jsx
+++ frontend/src/components/Test.jsx
@@ -1,21 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import toast from 'react-hot-toast';
 
 function Test() {
-    const [hasVisited, setHasVisited] = useState(false);
-
-    useEffect(() => {
-        if (!hasVisited) {
-            console.log('Welcome to this page for the first time!');
-            toast.success('message');
-            setHasVisited(true);
-        }
-    }, [hasVisited]); // The dependency array includes hasVisited so the effect runs when it changes
 
     return (
         <div>
             <h1>Welcome!</h1>
             <p>This is my component.</p>
+            <p>Starting work with typeScript ....hoorraaaaa</p>
         </div>
     );
 }
